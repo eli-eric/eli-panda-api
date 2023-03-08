0ee47b26f8ec6877fa788a39fb1e5357e871642a
JiriSvachaEliBeams <jiri.svacha@eli-beams.eu>
2023-03-08 11:31:01 +0100

    add authorization to systems routes

--- services/systems-service/systems-routes.go
+++ services/systems-service/systems-routes.go
@@ -2,18 +2,21 @@ package systemsService
 
 import (
 	"github.com/labstack/echo/v4"
+
+	m "panda/apigateway/middlewares"
+	"panda/apigateway/shared"
 )
 
 func MapSystemsRoutes(e *echo.Echo, h ISystemsHandlers, jwtMiddleware echo.MiddlewareFunc) {
 	// get all subsystems for given parent system spec. by parentUID
 	// if no parentUID is presented then get all root systems
-	e.GET("/v1/system/subsystems/:parentUID", h.GetSubSystemsByParentUID(), jwtMiddleware)
-	e.GET("/v1/system/subsystems/", h.GetSubSystemsByParentUID(), jwtMiddleware)
-	e.GET("/v1/system/subsystems", h.GetSubSystemsByParentUID(), jwtMiddleware)
+	e.GET("/v1/system/subsystems/:parentUID", m.Authorization(h.GetSubSystemsByParentUID(), shared.ROLE_SYSTEMS_VIEW), jwtMiddleware)
+	e.GET("/v1/system/subsystems/", m.Authorization(h.GetSubSystemsByParentUID(), shared.ROLE_SYSTEMS_VIEW), jwtMiddleware)
+	e.GET("/v1/system/subsystems", m.Authorization(h.GetSubSystemsByParentUID(), shared.ROLE_SYSTEMS_VIEW), jwtMiddleware)
 
 	//get system image - base64string
-	e.GET("/v1/system/:uid/image", h.GetSystemImageByUid(), jwtMiddleware)
+	e.GET("/v1/system/:uid/image", m.Authorization(h.GetSystemImageByUid(), shared.ROLE_SYSTEMS_VIEW), jwtMiddleware)
 
 	// get system detail by uid
-	e.GET("/v1/system/:uid", h.GetSystemDetail(), jwtMiddleware)
+	e.GET("/v1/system/:uid", m.Authorization(h.GetSystemDetail(), shared.ROLE_SYSTEMS_VIEW), jwtMiddleware)
 }
