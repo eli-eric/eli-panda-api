aa47efec73e06232110119c51ee30291fd41b25c
Jiri Svacha <jiri.svacha@eli-beams.eu>
2023-05-02 14:42:32 +0200

    fix get order EP

--- services/orders-service/models/orders-models.go
+++ services/orders-service/models/orders-models.go
@@ -20,23 +20,23 @@ type OrderListItem struct {
 }
 
 type OrderDetail struct {
-	UID            string                  `json:"uid"`
-	Name           string                  `json:"name"`
-	RequestNumber  string                  `json:"requestNumber"`
-	OrderNumber    string                  `json:"orderNumber"`
-	ContractNumber string                  `json:"contractNumber"`
-	Notes          string                  `json:"notes"`
-	Supplier       codebookModels.Codebook `json:"supplier"`
-	OrderStatus    codebookModels.Codebook `json:"orderStatus"`
-	OrderDate      time.Time               `json:"orderDate"`
-	OrderLines     []OrderLine             `json:"orderLines"`
+	UID            string                   `json:"uid"`
+	Name           string                   `json:"name"`
+	RequestNumber  string                   `json:"requestNumber"`
+	OrderNumber    string                   `json:"orderNumber"`
+	ContractNumber string                   `json:"contractNumber"`
+	Notes          string                   `json:"notes"`
+	Supplier       *codebookModels.Codebook `json:"supplier"`
+	OrderStatus    *codebookModels.Codebook `json:"orderStatus"`
+	OrderDate      time.Time                `json:"orderDate"`
+	OrderLines     []OrderLine              `json:"orderLines"`
 }
 
 type OrderLine struct {
-	Name            string                  `json:"name"`
-	UID             string                  `json:"uid"`
-	CatalogueNumber string                  `json:"catalogueNumber"`
-	CatalogueUID    string                  `json:"catalogueUid"`
-	System          codebookModels.Codebook `json:"system"`
-	PriceEUR        float64                 `json:"priceEur"`
+	Name            string                   `json:"name"`
+	UID             string                   `json:"uid"`
+	CatalogueNumber string                   `json:"catalogueNumber"`
+	CatalogueUID    string                   `json:"catalogueUid"`
+	System          *codebookModels.Codebook `json:"system"`
+	PriceEUR        float64                  `json:"priceEur"`
 }
