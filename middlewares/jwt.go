ba908334a23a1a6372290a4ccfe1c9f9bd652ad3
Jiri Svacha <jiri.svacha@eli-beams.eu>
2023-05-31 16:39:19 +0200

    set jwt secret

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -1,22 +1,28 @@
 package middlewares
 
 import (
-	"fmt"
+	"encoding/base64"
 	"panda/apigateway/services/security-service/models"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog/log"
 )
 
 func JwtMiddleware(jwtSecret string) echo.MiddlewareFunc {
 
 	// JWT middleware - Configure middleware with the custom claims type
+
+	jwtSecretDecoded, _ := base64.RawStdEncoding.DecodeString(jwtSecret)
+	stringjwt := string(jwtSecretDecoded)
+	log.Info().Msg(stringjwt)
+
 	jwtMiddlewareConfig := middleware.JWTConfig{
 		Claims:     &models.JwtCustomClaims{},
-		SigningKey: []byte(jwtSecret),
+		SigningKey: jwtSecretDecoded,
 		ErrorHandler: func(err error) error {
 			if err != nil {
-				fmt.Println(err)
+				log.Error().Msg(err.Error())
 				return echo.ErrUnauthorized
 			} else {
 				return nil
