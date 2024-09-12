4feb689319c9e54a308a50c08f57dbe2c4d15960
JiriSvachaEliBeams <jiri.svacha@eli-beams.eu>
2024-09-12 10:15:57 +0200

    general raphs uid restriction not null

--- services/general/general-db-queries.go
+++ services/general/general-db-queries.go
@@ -5,7 +5,7 @@ import (
 )
 
 func GetGraphNodesByUidQuery(uid string) (result helpers.DatabaseQuery) {
-	result.Query = `match(n{uid:$uid})-[r]-(o) RETURN DISTINCT {
+	result.Query = `match(n{uid:$uid})-[r]-(o) WHERE o.uid IS NOT NULL RETURN DISTINCT {
 	 uid: o.uid, 
 	 name: o.name, 
 	 label: labels(o)[0], 
@@ -25,9 +25,9 @@ func GetGraphNodesByUidQuery(uid string) (result helpers.DatabaseQuery) {
 
 func GetGraphLinksByUidQuery(uid string) (result helpers.DatabaseQuery) {
 	result.Query = `
-	match(n{uid:$uid})-[r]->(o) RETURN DISTINCT { source: n.uid, target: o.uid, relationship: type(r) } as links
+	match(n{uid:$uid})-[r]->(o) WHERE o.uid IS NOT NULL RETURN DISTINCT { source: n.uid, target: o.uid, relationship: type(r) } as links
 	union all
-	match(n{uid:$uid})<-[r]-(o) RETURN DISTINCT { source: o.uid, target: n.uid, relationship: type(r) } as links`
+	match(n{uid:$uid})<-[r]-(o) WHERE o.uid IS NOT NULL RETURN DISTINCT { source: o.uid, target: n.uid, relationship: type(r) } as links`
 	result.Parameters = make(map[string]interface{})
 	result.Parameters["uid"] = uid
 	result.ReturnAlias = "links"
