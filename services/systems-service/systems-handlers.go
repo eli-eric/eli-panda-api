f0d83142dc0d273414362ac316b95728768dcd05
Jiri Svacha <jiri.svacha@eli-beams.eu>
2024-09-24 13:22:50 +0200

    systems export to csv

--- services/systems-service/systems-handlers.go
+++ services/systems-service/systems-handlers.go
@@ -649,15 +649,145 @@ func (h *SystemsHandlers) GetSystemAsCsv() echo.HandlerFunc {
 
 			writer := csv.NewWriter(c.Response())
 			writer.UseCRLF = true
-			writer.Comma = ';'
+			writer.Comma = ','
 
 			defer writer.Flush()
-
+			// write header based on System struct
 			// write header
-			writer.Write([]string{"UID", "Name"})
+			writer.Write([]string{
+				"Name",
+				"SystemType",
+				"SystemCode",
+				"Zone",
+				"Location",
+				"Importance",
+				"EUN",
+				"SerialNumber",
+				"ItemUsage",
+				"SparePartsCoverage",
+				"MinimumSpareParts",
+				"CatalogueNumber",
+				"CatalogueCategory",
+				"Supplier",
+				"Owner",
+				"Responsible",
+				"ParentPath",
+				"Description",
+				"HasSubsystems",
+				"SystemLevel",
+				"OrderNumber",
+				"OrderUid",
+				"UID"})
 
 			for _, item := range items.Data {
-				writer.Write([]string{item.UID, item.Name})
+				// construct parent path string
+				parentPath := ""
+				for ip, parent := range item.ParentPath {
+					if ip > 0 {
+						parentPath += " > "
+					}
+					parentPath += parent.Name
+				}
+				systemType := ""
+				if item.SystemType != nil {
+					systemType = item.SystemType.Name
+				}
+				description := ""
+				if item.Description != nil {
+					description = *item.Description
+				}
+				systemCode := ""
+				if item.SystemCode != nil {
+					systemCode = *item.SystemCode
+				}
+				zone := ""
+				if item.Zone != nil {
+					zone = item.Zone.Code
+				}
+				location := ""
+				if item.Location != nil {
+					location = item.Location.Code
+				}
+				systemLevel := ""
+				if item.SystemLevel != nil {
+					systemLevel = *item.SystemLevel
+				}
+				owner := ""
+				if item.Owner != nil {
+					owner = item.Owner.Name
+				}
+				responsible := ""
+				if item.Responsible != nil {
+					responsible = item.Responsible.Name
+				}
+				importance := ""
+				if item.Importance != nil {
+					importance = item.Importance.Name
+				}
+				eun := ""
+				if item.PhysicalItem != nil && item.PhysicalItem.EUN != nil {
+					eun = *item.PhysicalItem.EUN
+				}
+				serialNumber := ""
+				if item.PhysicalItem != nil && item.PhysicalItem.SerialNumber != nil {
+					serialNumber = *item.PhysicalItem.SerialNumber
+				}
+				itemUsage := ""
+				if item.PhysicalItem != nil && item.PhysicalItem.ItemUsage != nil {
+					itemUsage = item.PhysicalItem.ItemUsage.Name
+				}
+				catalogueNumber := ""
+				if item.PhysicalItem != nil {
+					catalogueNumber = item.PhysicalItem.CatalogueItem.CatalogueNumber
+				}
+				catalogueCategory := ""
+				if item.PhysicalItem != nil {
+					catalogueCategory = item.PhysicalItem.CatalogueItem.Category.Name
+				}
+				orderNumber := ""
+				if item.PhysicalItem != nil && item.PhysicalItem.OrderNumber != nil {
+					orderNumber = *item.PhysicalItem.OrderNumber
+				}
+				orderUid := ""
+				if item.PhysicalItem != nil && item.PhysicalItem.OrderUid != nil {
+					orderUid = *item.PhysicalItem.OrderUid
+				}
+				supplier := ""
+				if item.PhysicalItem != nil && item.PhysicalItem.CatalogueItem.Supplier != nil {
+					supplier = item.PhysicalItem.CatalogueItem.Supplier.Name
+				}
+				sparePartsCoverage := ""
+				if item.Statistics != nil && item.Statistics.Sp_coverage != nil {
+					sparePartsCoverage = strconv.FormatFloat(float64(*item.Statistics.Sp_coverage), 'f', -1, 32)
+				}
+				minimumSpareParts := ""
+				if item.Statistics != nil && item.Statistics.MinimalSpareParstCount != nil {
+					minimumSpareParts = strconv.FormatFloat(float64(*item.Statistics.MinimalSpareParstCount), 'f', -1, 32)
+				}
+				writer.Write([]string{
+					item.Name,
+					systemType,
+					systemCode,
+					zone,
+					location,
+					importance,
+					eun,
+					serialNumber,
+					itemUsage,
+					sparePartsCoverage,
+					minimumSpareParts,
+					catalogueNumber,
+					catalogueCategory,
+					supplier,
+					owner,
+					responsible,
+					parentPath,
+					description,
+					strconv.FormatBool(item.HasSubsystems),
+					systemLevel,
+					orderNumber,
+					orderUid,
+					item.UID})
 			}
 
 			return nil
