c053d62fe861fdbbd7360fad278796d472f45152
Jan Smrcka <jan.smrck@gmail.com>
2026-03-18 14:19:48 +0100

    feat(zones): add route registration

---
+++ services/zone-service/zone-routes.go
@@ -0,0 +1,17 @@
+package zoneservice
+
+import (
+	m "panda/apigateway/middlewares"
+	"panda/apigateway/shared"
+
+	"github.com/labstack/echo/v4"
+)
+
+func MapZoneRoutes(e *echo.Echo, h IZoneHandlers, jwtMiddleware echo.MiddlewareFunc) {
+	e.GET("/v1/zones", m.Authorization(h.GetAllZones(), shared.ROLE_SYSTEMS_VIEW), jwtMiddleware)
+	e.GET("/v1/zones/:uid", m.Authorization(h.GetZoneByUID(), shared.ROLE_SYSTEMS_VIEW), jwtMiddleware)
+	e.POST("/v1/zones", m.Authorization(h.CreateZone(), shared.ROLE_CODEBOOKS_ADMIN), jwtMiddleware)
+	e.PUT("/v1/zones/:uid", m.Authorization(h.UpdateZone(), shared.ROLE_CODEBOOKS_ADMIN), jwtMiddleware)
+	e.DELETE("/v1/zones/:uid", m.Authorization(h.DeleteZone(), shared.ROLE_CODEBOOKS_ADMIN), jwtMiddleware)
+	e.POST("/v1/zones/import", m.Authorization(h.ImportZones(), shared.ROLE_CODEBOOKS_ADMIN), jwtMiddleware)
+}
